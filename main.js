b96c59449e9cdd5c86c5e88166a556809fc9aead
andis03 <43890472+andis03@users.noreply.github.com>
2021-12-10 15:02:05 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,19 @@
+import 'ol/ol.css';
+import Map from 'ol/Map';
+import OSM from 'ol/source/OSM';
+import TileLayer from 'ol/layer/Tile';
+import View from 'ol/View';
+
+const map = new Map({
+  layers: [
+    new TileLayer({
+      source: new OSM(),
+    }),
+  ],
+  target: 'map',
+  view: new View({
+    center: [14200000, 4130000],
+    rotation: Math.PI / 6,
+    zoom: 10,
+  }),
+});
